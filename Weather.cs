d629670efff1f2da5f30b47194288aba2fa925c0
Muhammad Ali <alinaeem229@gmail.com>
2022-04-09 16:43:08 -0400

    Get coordinates of city

--- Weather.cs
+++ Weather.cs
@@ -11,9 +11,11 @@ namespace WeTile
         public string City { get; set; }
         public bool Connection;
         XmlDocument Doc = new XmlDocument();
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public double Temperature { get; set; }
         public DateTime Timestamp { get; set; }
-        public string Url { get; set; }
+        public string URL { get; set; }
         public string WeatherIcon { get; set; }
         public string WeatherName { get; set; }
         public string XML { get; set; }
@@ -22,17 +24,19 @@ namespace WeTile
         {
 
             City = Properties.Settings.Default.citySetting;
-            Url = "http://api.openweathermap.org/data/2.5/weather?q=" + City + "&APPID=a1916e5365462ceb65cfa9bb0606d1d8&mode=xml&units="
+            URL = "http://api.openweathermap.org/data/2.5/weather?q=" + City + "&APPID=a1916e5365462ceb65cfa9bb0606d1d8&mode=xml&units="
             + (Properties.Settings.Default.unitSetting == "C" ? "metric" : "imperial");
-            Console.WriteLine(Url);
-            XML = await new WebClient().DownloadStringTaskAsync(new Uri(Url));
+            XML = await new WebClient().DownloadStringTaskAsync(new Uri(URL));
             Doc.LoadXml(XML);
             Timestamp = DateTime.Now;
 
-            Temperature = double.Parse(Doc.DocumentElement.SelectSingleNode("temperature").Attributes["value"].Value);
-            WeatherName = Doc.DocumentElement.SelectSingleNode("weather").Attributes["value"].Value;
-            City = Doc.DocumentElement.SelectSingleNode("city").Attributes["name"].Value;
-            WeatherIcon = Doc.DocumentElement.SelectSingleNode("weather").Attributes["number"].Value;
+            XmlElement Element = Doc.DocumentElement;
+            Temperature = double.Parse(Element.SelectSingleNode("temperature").Attributes["value"].Value);
+            WeatherName = Element.SelectSingleNode("weather").Attributes["value"].Value;
+            City = Element.SelectSingleNode("city").Attributes["name"].Value;
+            WeatherIcon = Element.SelectSingleNode("weather").Attributes["number"].Value;
+            Latitude = double.Parse(Element.SelectSingleNode("city/coord").Attributes["lat"].Value);
+            Longitude = double.Parse(Element.SelectSingleNode("city/coord").Attributes["lon"].Value);
 
             await Task.Delay(1000);
             return Connection = true;
