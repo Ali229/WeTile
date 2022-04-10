9e1d801008de51087e2a1ff01c157e26cb6fc85d
Muhammad Ali <alinaeem229@gmail.com>
2022-04-10 07:59:46 -0400

    Added newtonsoft package, add air quality data

--- AirQuality.cs
+++ AirQuality.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Net;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Xml;
 
 namespace WeTile
 {
     internal class AirQuality
     {
-        public string URL { get; set; }
-        public string XML { get; set; }
+        public Coord Coord { get; set; }
+        public List[] List { get; set; }
 
-        XmlDocument Doc = new XmlDocument();
         public async Task<bool> GetAirQuality(double Latitude, double Longitude)
         {
             try
             {
-                using (WebClient webClient = new WebClient())
-                {
-                    webClient.BaseAddress = String.Format("http://api.openweathermap.org/data/2.5/air_pollution?lat={0}&lon={1}&appid={2}", Latitude, Longitude, "a1916e5365462ceb65cfa9bb0606d1d8");
-                    string JSON = webClient.DownloadString("");
+                HttpClient client = new HttpClient();
+                string URL = String.Format("http://api.openweathermap.org/data/2.5/air_pollution?lat={0}&lon={1}&appid={2}", Latitude, Longitude, "a1916e5365462ceb65cfa9bb0606d1d8");
+                string reponse = await client.GetStringAsync(URL);
 
-
-                }
+                var Air2 = JsonConvert.DeserializeObject<AirQuality>(reponse);
                 return true;
             }
             catch (Exception e)
@@ -29,5 +26,33 @@ namespace WeTile
                 return false;
             }
         }
+    }
+    public class Coord
+    {
+        public float Lon { get; set; }
+        public float Lat { get; set; }
+    }
+
+    public class List
+    {
+        public Main Main { get; set; }
+        public Components Components { get; set; }
+        public int DT { get; set; }
+    }
+
+    public class Main
+    {
+        public int AQI { get; set; }
+    }
+    public class Components
+    {
+        public float CO { get; set; }
+        public float NO { get; set; }
+        public float NO2 { get; set; }
+        public float O3 { get; set; }
+        public float SO2 { get; set; }
+        public float PM2_5 { get; set; }
+        public float PM10 { get; set; }
+        public float NH3 { get; set; }
     }
 }
